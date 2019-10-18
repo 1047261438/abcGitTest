e904673dbe5d5cbc5279170633db2ab2931d3e31
mySpringBoot <wq60833@qq.com>
2019-10-18 14:42:59 +0800

    添加关于提交给GitHub的基础代码结构

---
+++ src/main/java/abc.java
@@ -0,0 +1,11 @@
+/**
+ * @Auther:http://www.baidu.com
+ * @Date:2019/10/18
+ * @Description:PACKAGE_NAME
+ * @version:1.0
+ */
+public class abc {
+    public static void main(String[] args) {
+        System.out.println("abc!");
+    }
+}
